cd46e078feccdb8d0ccaa1901d53925aee204c01
Derek Whittom <dwhittom@talentwise.com>
2016-04-03 05:14:32 -0700

    Added config.m4 and travis build

--- .travis.scripts/compile.sh
+++ .travis.scripts/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+CACHE_DIR=$1
+CZMQ_PREFIX="${CACHE_DIR}/czmq-libzmq"
 install_czmq() {
-    local version=$1
     local build_dir="${BUILD_DIR}/czmq-build"
 
     if test -d "${build_dir}"
@@ -16,7 +16,7 @@ install_czmq() {
         ./autogen.sh
 
 
-        ./configure --prefix="/usr/lib/czmq" --without-libsodium
+        ./configure --prefix="${CZMQ_PREFIX}" --without-libsodium
 
         make -j 8
         make install
@@ -27,7 +27,7 @@ BUILD_DIR="/tmp"
 export CFLAGS="-Wall -Wextra -Wmissing-field-initializers -Wno-unused-parameter -ggdb3"
 phpize
 install_czmq
-./configure --with-zmq --with-czmq=/usr/lib/czmq
+./configure --with-zmq --with-czmq=${CZMQ_PREFIX}
 make all install
 EXTENSIONDIR=`php -r 'echo ini_get("extension_dir");'`
 echo "zend_extension=${EXTENSIONDIR}/php_profiler.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
