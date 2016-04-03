98edc62b435900f4d344fae2ba0358ecae86cc29
Derek Whittom <dwhittom@talentwise.com>
2016-04-03 03:09:21 -0700

    Added config.m4 and travis build

--- php_profiler.c
+++ php_profiler.c
@@ -9,6 +9,71 @@
 #include "zend_extensions.h"
 #include "czmq/include/czmq.h"
 
+void activate_function(zend_extension *extension) {
+    zctx_t *ctx = zctx_new ();
+    printf ("I: connecting to server...\n");
+    void *client = zsocket_new (ctx, ZMQ_REQ);
+    assert (client);
+    zsocket_connect (client, SERVER_ENDPOINT);
+
+    int sequence = 0;
+    int retries_left = REQUEST_RETRIES;
+    while (retries_left && !zctx_interrupted) {
+        //  We send a request, then we work to get a reply
+        char request [10];
+        sprintf (request, "%d", ++sequence);
+        zstr_send (client, request);
+
+        int expect_reply = 1;
+        while (expect_reply) {
+            //  Poll socket for a reply, with timeout
+            zmq_pollitem_t items [] = { { client, 0, ZMQ_POLLIN, 0 } };
+            int rc = zmq_poll (items, 1, REQUEST_TIMEOUT * ZMQ_POLL_MSEC);
+            if (rc == -1)
+                break;          //  Interrupted
+
+            //  .split process server reply
+            //  Here we process a server reply and exit our loop if the
+            //  reply is valid. If we didn't a reply we close the client
+            //  socket and resend the request. We try a number of times
+            //  before finally abandoning:
+
+            if (items [0].revents & ZMQ_POLLIN) {
+                //  We got a reply from the server, must match sequence
+                char *reply = zstr_recv (client);
+                if (!reply)
+                    break;      //  Interrupted
+                if (atoi (reply) == sequence) {
+                    printf ("I: server replied OK (%s)\n", reply);
+                    retries_left = REQUEST_RETRIES;
+                    expect_reply = 0;
+                }
+                else
+                    printf ("E: malformed reply from server: %s\n",
+                            reply);
+
+                free (reply);
+            }
+            else
+            if (--retries_left == 0) {
+                printf ("E: server seems to be offline, abandoning\n");
+                break;
+            }
+            else {
+                printf ("W: no response from server, retrying...\n");
+                //  Old socket is confused; close it and open a new one
+                zsocket_destroy (ctx, client);
+                printf ("I: reconnecting to server...\n");
+                client = zsocket_new (ctx, ZMQ_REQ);
+                zsocket_connect (client, SERVER_ENDPOINT);
+                //  Send request again, on new socket
+                zstr_send (client, request);
+            }
+        }
+    }
+    zctx_destroy (&ctx);
+    return 0;
+}
 void statement_handler(zend_op_array *op_array) {
     //TSRMLS_FETCH();
     //fprintf(stderr, "%s:%d\n", zend_get_executed_filename(TSRMLS_C), zend_get_executed_lineno(TSRMLS_C));
@@ -42,7 +107,7 @@ ZEND_DLEXPORT zend_extension zend_extension_entry = {
         "",
         call_coverage_zend_startup,
         NULL,
-        NULL,
+        activate_function,
         NULL,
         NULL,
         NULL,
